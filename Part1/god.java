fa028ff6bf7f4a5a5a6864b94c0c2cc3ae770653
Less <63160418+NLessW@users.noreply.github.com>
2023-04-04 12:05:00 +0900

    Create god.java

---
+++ Part1/god.java
@@ -0,0 +1,13 @@
+
+public class god {
+
+	public god(){
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person a1 = new Person("Kim",50);
+		a1.age=100;
+	}
+
+}
